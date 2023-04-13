a66209455c30f95eda1a8f545d2e8d8ee843624f
matheusbortolottibruno <110576109+matheusbortolottibruno@users.noreply.github.com>
2023-04-12 22:12:22 -0300

    AVALIACAO

--- index.php/programa.php
+++ index.php/programa.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Faça o programa </title>
 </head>
-<body>
-    
-<?php 
 
-/*Faça o programa conforme os prints anexos:
+<body>
+
+    <?php
+
+    /*Faça o programa conforme os prints anexos:
     em um arquivo ter os campos
     e no outro arquivo apresentar o que foi digitado nos campos*/
-?>
+    ?>
 
 
-<form action="recebe.php" method="post">
-Nome: <input name="nome"> <br><br>
-Profissão: <input name="profissao"> <br><br>
+    <form action="recebe.php" method="post">
+        Nome: <input name="nome"> <br><br>
+        Profissão: <input name="profissao"> <br><br>
 
-<button type="submit">Salvar</button>
+        <button type="submit">Salvar</button>
 
 
-</form>
+    </form>
 </body>
+
 </html>
